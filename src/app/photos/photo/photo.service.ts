d1e9f09a72163afd43f0bcd22251f8cf313250e2
Thiago Bussola <thiago.silva@sabium.com.br>
2019-09-06 11:08:33 -0300

    Termino modulo 6

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Photo } from './photo';
 
 const API = 'http://localhost:3000'
@@ -10,10 +10,18 @@ const API = 'http://localhost:3000'
 export class PhotoService {
     
     constructor(private http: HttpClient) { }
-
+    
     listFromUser(userName: string) {
         return this.http.get<Photo[]>(API+ '/' + userName + '/photos');
-
+        
     }
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+            .append('page', page.toString());
 
+        return this.http
+            .get<Photo[]>(API+ '/' + userName + '/photos', { params });
+        
+    }
+    
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,13 +5,17 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
     declarations: [
         PhotoComponent,
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescription,
+        LoadButtonComponent
     ],
     exports: [
         PhotoComponent,
