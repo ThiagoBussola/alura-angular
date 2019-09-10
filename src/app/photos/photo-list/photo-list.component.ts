11a216204832159a6faa80635abce562afe55f25
Thiago Bussola <thiago.silva@sabium.com.br>
2019-09-10 11:23:58 -0300

    Finalização do curso 1

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -16,7 +14,6 @@ export class PhotoListComponent implements OnInit {
 
     photos: Photo[] = [];
     filter: string = '';
-    debounce: Subject<string> = new Subject<string>();
     hasMore: boolean = true;
     currentPage: number = 1;
     userName: string = '';
@@ -29,13 +26,6 @@ export class PhotoListComponent implements OnInit {
     ngOnInit(): void {
         this.userName = this.activatedRoute.snapshot.params.userName;
         this.photos = this.activatedRoute.snapshot.data['photos'];
-        this.debounce
-            .pipe(debounceTime(300))
-            .subscribe(filter => this.filter = filter);
-    }
-
-    ngOnDestroy(): void {
-        this.debounce.unsubscribe();
         
     }
 
@@ -43,6 +33,7 @@ export class PhotoListComponent implements OnInit {
         this.photoService
             .listFromUserPaginated(this.userName, ++this.currentPage)
             .subscribe(photos => {
+                this.filter = '';
                 this.photos = this.photos.concat(photos);
                 // se não veio nenhum dado o botão vai sumir
                 if(!photos.length) this.hasMore = false;
