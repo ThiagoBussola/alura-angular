00989015357f5aad0f6d4b113ba8bb0e2240bd96
Thiago Bussola <thiago.silva@sabium.com.br>
2019-09-26 09:27:38 -0300

    singup inicio

--- src/app/home/singup/signup.component.ts
+++ src/app/home/singup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -9,5 +9,39 @@ export class SignUpComponent {
     signupForm: FormGroup;
 
     constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.signupForm = this.formBuilder.group({
+            email: ['', 
+                [
+                    Validators.required, 
+                    Validators.email
+                ]
+            ],
+            userName: ['', 
+                [
+                    Validators.required,
+                    Validators.pattern(/^[a-z0-9_\-]+$/),
+                    Validators.minLength(2), 
+                    Validators.maxLength(40)
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required, 
+                    Validators.minLength(2), 
+                    Validators.maxLength(30)
+                ]
+            ],
+            password: ['', 
+                [
+                    Validators.required, 
+                    Validators.minLength(8), 
+                    Validators.maxLength(14)
+                ]
+            ]
+        })
+        
+    }
 
 }
